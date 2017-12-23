9afba4eb15ad5d89922d3b9260f2b5341f32ba77
Kobnar <konrad.rk.ludwig@gmail.com>
2017-12-23 09:11:03 -0800

    Add tests for testing utility functions

--- stackcite/users/testing/tests/test_utils.py
+++ stackcite/users/testing/tests/test_utils.py
@@ -21,3 +21,92 @@ class CreateRequestTestCase(unittest.TestCase):
         headers = request.headers
         result = headers['Authorization']
         self.assertEqual(result, key)
+
+
+class CreateUserTestCase(unittest.TestCase):
+
+    layer = testing.layers.MongoTestLayer
+
+    def setUp(self):
+        from stackcite.users import models
+        models.User.drop_collection()
+
+    def test_create_user_does_not_save_user(self):
+        from .. import utils
+        test_email = 'test@email.com'
+        utils.create_user(test_email, 'T3stPa$$word', save=False)
+        from stackcite.users import models
+        import mongoengine
+        with self.assertRaises(mongoengine.DoesNotExist):
+            models.User.objects.get(email=test_email)
+
+    def test_create_user_saves_user(self):
+        from .. import utils
+        test_email = 'test@email.com'
+        utils.create_user(test_email, 'T3stPa$$word', save=True)
+        from stackcite.users import models
+        import mongoengine
+        try:
+            models.User.objects.get(email=test_email)
+        except mongoengine.DoesNotExist as err:
+            self.fail(msg=err)
+
+
+class CreateAuthTokenTestCase(unittest.TestCase):
+
+    layer = testing.layers.MongoTestLayer
+
+    def setUp(self):
+        from stackcite.users import models
+        models.User.drop_collection()
+
+    def test_create_auth_token_does_not_save_user(self):
+        from stackcite.users import models
+        user = models.User.new('test@email.com', 'T3stPa$$word', save=True)
+        from .. import utils
+        utils.create_auth_token(user)
+        import mongoengine
+        with self.assertRaises(mongoengine.DoesNotExist):
+            models.AuthToken.objects.get(_user=user)
+
+    def test_create_auth_token_saves_user(self):
+        from stackcite.users import models
+        user = models.User.new('test@email.com', 'T3stPa$$word', save=True)
+        from .. import utils
+        utils.create_auth_token(user, save=True)
+        from stackcite.users import models
+        import mongoengine
+        try:
+            models.AuthToken.objects.get(_user=user)
+        except mongoengine.DoesNotExist as err:
+            self.fail(msg=err)
+
+
+class CreateConfTokenTestCase(unittest.TestCase):
+
+    layer = testing.layers.MongoTestLayer
+
+    def setUp(self):
+        from stackcite.users import models
+        models.User.drop_collection()
+
+    def test_create_conf_token_does_not_save_user(self):
+        from stackcite.users import models
+        user = models.User.new('test@email.com', 'T3stPa$$word', save=True)
+        from .. import utils
+        utils.create_conf_token(user)
+        import mongoengine
+        with self.assertRaises(mongoengine.DoesNotExist):
+            models.ConfirmToken.objects.get(_user=user)
+
+    def test_create_conf_token_saves_user(self):
+        from stackcite.users import models
+        user = models.User.new('test@email.com', 'T3stPa$$word', save=True)
+        from .. import utils
+        utils.create_conf_token(user, save=True)
+        from stackcite.users import models
+        import mongoengine
+        try:
+            models.ConfirmToken.objects.get(_user=user)
+        except mongoengine.DoesNotExist as err:
+            self.fail(msg=err)
